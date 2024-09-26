5bbc957ab8c0ca6aef32c3d8e3a61248dc99ba83
hamzagedikkaya <gedikkayahamza@gmail.com>
2024-09-26 22:50:52 +0300

    Add custom fields for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     [ "email" ]
   end
+
+  attr_accessor :first_name, :last_name
+  before_save :combine_names
+  validates :first_name, :last_name, :email, :password, presence: true
+
+  private
+
+  def combine_names
+    byebug
+    self.name_surname = "#{first_name} #{last_name}".strip
+  end
 end
